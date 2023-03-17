a182bffc3cdf7e96c8e5aff5f5457bd232ffc08e
Yeampier2022 <yeampierjesusa@gmail.com>
2023-03-17 11:45:57 -0400

    validar user

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -12,6 +12,10 @@ import { AuthService } from '@services/auth.service';
   templateUrl: './register-form.component.html',
 })
 export class RegisterFormComponent {
+  formUser = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+  });
+
   form = this.formBuilder.nonNullable.group(
     {
       name: ['', [Validators.required]],
@@ -26,9 +30,12 @@ export class RegisterFormComponent {
     }
   );
   status: RequestStatus = 'init';
+  statusUser: RequestStatus = 'init';
+
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  showRegister = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -39,19 +46,45 @@ export class RegisterFormComponent {
   register() {
     if (this.form.valid) {
       this.status = 'loading';
-      const { name, email, password } = this.form.getRawValue();
-      this.authService.register(name, email, password)
-      .subscribe({
+      const { email, name, password } = this.form.getRawValue();
+      this.authService.register(email, name, password).subscribe({
         next: () => {
           this.status = 'success';
           this.router.navigate(['/login']);
         },
-        error: () => {
+        error: (error) => {
           this.status = 'failed';
+          console.log(error);
         },
       });
     } else {
       this.form.markAllAsTouched();
+    }
+  }
+
+  validateUser() {
+    if (this.formUser.valid) {
+      this.statusUser = 'loading';
+      const { email } = this.formUser.getRawValue();
+      this.authService.isAvailable(email).subscribe({
+        next: (rta) => {
+          this.statusUser = 'success';
+          if (rta.isAvailable) {
+            this.showRegister = true;
+            this.form.controls.email.setValue(email);
+          } else {
+            this.router.navigate(['/login'], {
+              queryParams: { email },
+            });
+          }
+        },
+        error: (error) => {
+          this.statusUser = 'failed';
+          console.log(error);
+        },
+      });
+    } else {
+      this.formUser.markAllAsTouched();
     }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,11 +17,15 @@ export class AuthService {
     });
   }
 
-  register(name: string, password: string, email: string) {
+  register(email: string, name: string, password: string) {
     return this.http.post(`${this.apiUrl}/api/v1/auth/register`, {
       name,
       email,
       password,
     });
+  }
+
+  isAvailable(email: string) {
+    return this.http.post<{isAvailable: boolean }>(`${this.apiUrl}/api/v1/auth/is-available`, { email });
   }
 }

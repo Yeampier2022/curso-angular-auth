24557b8298f5f8b6ccfae7ef6a2fab2d91c66f32
Yeampier2022 <yeampierjesusa@gmail.com>
2023-03-22 10:15:33 -0400

    Proteger el Acceso

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,20 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { TokenService } from './token.service';
+import {ResponseLogin} from '@models/auth.models'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   apiUrl = environment.Api_URL;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private tokenServices: TokenService) {}
 
   login(email: string, password: string) {
-    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {
+    return this.http.post<ResponseLogin>(`${this.apiUrl}/api/v1/auth/login`, {
       email,
       password,
-    });
+    }).pipe(
+      tap(response => {
+        this.tokenServices.saveToken(response.access_token)
+      } )
+    )
   }
 
   register(email: string, name: string, password: string) {

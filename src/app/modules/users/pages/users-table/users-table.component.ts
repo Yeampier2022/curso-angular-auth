20e94f2184a7657feb20291ea964540a076a06b5
Yeampier2022 <yeampierjesusa@gmail.com>
2023-03-22 17:28:38 -0400

    reactividad en profile

--- src/app/modules/users/pages/users-table/users-table.component.ts
+++ src/app/modules/users/pages/users-table/users-table.component.ts
@@ -4,6 +4,9 @@ import { DataSourceUser } from './data-source';
 
 import { UsersService } from '@services/users.service';
 
+import { AuthService } from '@services/auth.service';
+import { User } from '@models/users.models';
+
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
@@ -11,12 +14,20 @@ import { UsersService } from '@services/users.service';
 export class UsersTableComponent implements OnInit {
   dataSource = new DataSourceUser();
   columns: string[] = ['id', 'avatar', 'name', 'email'];
+  user: User | null = null;
 
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.usersService.getUsers().subscribe((users) => {
       this.dataSource.init(users);
     });
+   this.authService.user$.subscribe(user =>{
+    this.user = user
+   });
+    ;
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,26 +4,33 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { switchMap, tap } from 'rxjs/operators';
 import { TokenService } from './token.service';
-import {ResponseLogin} from '@models/auth.models'
+import { ResponseLogin } from '@models/auth.models';
 import { User } from '@models/users.models';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   apiUrl = environment.Api_URL;
-  constructor(private http: HttpClient,
-    private tokenServices: TokenService) {}
+  user$ = new BehaviorSubject<User | null>(null)
+
+getDataUser() {
+  return this.user$.getValue()
+}
+  constructor(private http: HttpClient, private tokenServices: TokenService) {}
 
   login(email: string, password: string) {
-    return this.http.post<ResponseLogin>(`${this.apiUrl}/api/v1/auth/login`, {
-      email,
-      password,
-    }).pipe(
-      tap(response => {
-        this.tokenServices.saveToken(response.access_token)
-      } )
-    )
+    return this.http
+      .post<ResponseLogin>(`${this.apiUrl}/api/v1/auth/login`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          this.tokenServices.saveToken(response.access_token);
+        })
+      );
   }
 
   register(email: string, name: string, password: string) {
@@ -57,16 +64,20 @@ export class AuthService {
     });
   }
 
-  getProfile()  {
+  getProfile() {
     const token = this.tokenServices.getToken();
     return this.http.get<User>(`${this.apiUrl}/api/v1/auth/profile`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    });
+    }).pipe(tap(
+      user => {
+        this.user$.next(user)
+      }
+    ))
   }
 
   logout() {
-    this.tokenServices.removeToken()
+    this.tokenServices.removeToken();
   }
 }
